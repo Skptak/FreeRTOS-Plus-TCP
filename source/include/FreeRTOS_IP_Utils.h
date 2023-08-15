d2862b6486c944b21b26a426b954e57f8457a55a
Soren Ptak <ptaksoren@gmail.com>
2023-08-15 16:39:54 -0700

    clang-format the FreeRTOS_IP_ Files

--- source/include/FreeRTOS_IP_Utils.h
+++ source/include/FreeRTOS_IP_Utils.h
@@ -4,22 +4,23 @@
  *
  * SPDX-License-Identifier: MIT
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy of
- * this software and associated documentation files (the "Software"), to deal in
- * the Software without restriction, including without limitation the rights to
- * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
- * the Software, and to permit persons to whom the Software is furnished to do so,
- * subject to the following conditions:
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
  *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
  *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
- * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
- * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
- * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
- * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  *
  * http://aws.amazon.com/freertos
  * http://www.FreeRTOS.org
@@ -61,14 +62,14 @@
 
 /* *INDENT-OFF* */
 #ifdef __cplusplus
-    extern "C" {
+extern "C" {
 #endif
 /* *INDENT-ON* */
 
 /* Forward declaration. */
 struct xNetworkInterface;
 
-#if ( ipconfigUSE_DHCP != 0 )
+#if( ipconfigUSE_DHCP != 0 )
 
 /**
  * @brief Create a DHCP event.
@@ -76,24 +77,26 @@ struct xNetworkInterface;
  * @return pdPASS or pdFAIL, depending on whether xSendEventStructToIPTask()
  *         succeeded.
  */
-    BaseType_t xSendDHCPEvent( struct xNetworkEndPoint * pxEndPoint );
+BaseType_t xSendDHCPEvent( struct xNetworkEndPoint * pxEndPoint );
 #endif
 
-#if ( ipconfigZERO_COPY_TX_DRIVER != 0 ) || ( ipconfigZERO_COPY_RX_DRIVER != 0 )
+#if( ipconfigZERO_COPY_TX_DRIVER != 0 ) || ( ipconfigZERO_COPY_RX_DRIVER != 0 )
 
 /**
  * @brief Get the network buffer from the packet buffer.
  *
  * @param[in] pvBuffer: Pointer to the packet buffer.
  *
- * @return The network buffer if the alignment is correct. Else a NULL is returned.
+ * @return The network buffer if the alignment is correct. Else a NULL is
+ * returned.
  */
-    NetworkBufferDescriptor_t * pxPacketBuffer_to_NetworkBuffer( const void * pvBuffer );
+NetworkBufferDescriptor_t * pxPacketBuffer_to_NetworkBuffer(
+    const void * pvBuffer );
 #endif
 
 /**
- * @brief Check the values of configuration options and assert on it. Also verify that the IP-task
- *        has not already been initialized.
+ * @brief Check the values of configuration options and assert on it. Also
+ * verify that the IP-task has not already been initialized.
  */
 void vPreCheckConfigs( void );
 
@@ -105,7 +108,7 @@ void prvProcessNetworkDownEvent( struct xNetworkInterface * pxInterface );
 
 /* *INDENT-OFF* */
 #ifdef __cplusplus
-    } /* extern "C" */
+} /* extern "C" */
 #endif
 /* *INDENT-ON* */
 
