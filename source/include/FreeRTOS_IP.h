b18286f36dfc2a284f1a92bcd89e545a46e4b6ca
Soren Ptak <ptaksoren@gmail.com>
2023-08-15 23:36:14 -0700

    Clang format FreeRTOS_IPInit()

--- source/include/FreeRTOS_IP.h
+++ source/include/FreeRTOS_IP.h
@@ -333,15 +333,17 @@ struct xNetworkInterface;
 struct xNetworkInterface * pxFillInterfaceDescriptor(
     BaseType_t xEMACIndex,
     struct xNetworkInterface * pxInterface );
-/* clang-format off */
-/* The following function is only provided to allow backward compatibility
- * with the earlier version of FreeRTOS+TCP which had a single interface only. */
-    BaseType_t FreeRTOS_IPInit( const uint8_t ucIPAddress[ ipIP_ADDRESS_LENGTH_BYTES ],
-                                const uint8_t ucNetMask[ ipIP_ADDRESS_LENGTH_BYTES ],
-                                const uint8_t ucGatewayAddress[ ipIP_ADDRESS_LENGTH_BYTES ],
-                                const uint8_t ucDNSServerAddress[ ipIP_ADDRESS_LENGTH_BYTES ],
-                                const uint8_t ucMACAddress[ ipMAC_ADDRESS_LENGTH_BYTES ] );
 
+/* The following function is only provided to allow backward compatibility
+ * with the earlier version of FreeRTOS+TCP which had a single interface only.
+ */
+BaseType_t FreeRTOS_IPInit(
+    const uint8_t ucIPAddress[ ipIP_ADDRESS_LENGTH_BYTES ],
+    const uint8_t ucNetMask[ ipIP_ADDRESS_LENGTH_BYTES ],
+    const uint8_t ucGatewayAddress[ ipIP_ADDRESS_LENGTH_BYTES ],
+    const uint8_t ucDNSServerAddress[ ipIP_ADDRESS_LENGTH_BYTES ],
+    const uint8_t ucMACAddress[ ipMAC_ADDRESS_LENGTH_BYTES ] );
+/* clang-format off */
 /* The following 2 functions also assume that there is only 1 network endpoint/interface.
  * The new function are called: FreeRTOS_GetEndPointConfiguration() and
  * FreeRTOS_SetEndPointConfiguration() */
