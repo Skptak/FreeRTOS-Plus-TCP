5e8ec573feebc4a50a8c5b4e8d1739a7811c833b
Soren Ptak <ptaksoren@gmail.com>
2023-08-15 23:50:38 -0700

    oh boy more guess and checking

--- source/include/FreeRTOS_IP.h
+++ source/include/FreeRTOS_IP.h
@@ -416,47 +416,46 @@ void FreeRTOS_UpdateMACAddress(
                                     uint16_t usIdentifier );
 #endif
 
+/* clang-format on */
 /* xARPWaitResolution checks if an IPv4 address is already known. If not
  * it may send an ARP request and wait for a reply.  This function will
  * only be called from an application. */
-BaseType_t xARPWaitResolution( uint32_t ulIPAddress,
-                               TickType_t uxTicksToWait );
+BaseType_t xARPWaitResolution( uint32_t ulIPAddress, TickType_t uxTicksToWait );
 
 BaseType_t FreeRTOS_IsNetworkUp( void );
 
-#if ( ipconfigCHECK_IP_QUEUE_SPACE != 0 )
-    UBaseType_t uxGetMinimumIPQueueSpace( void );
+#if( ipconfigCHECK_IP_QUEUE_SPACE != 0 )
+UBaseType_t uxGetMinimumIPQueueSpace( void );
 #endif
 
 BaseType_t xIsNetworkDownEventPending( void );
 
 /*
  * Defined in FreeRTOS_Sockets.c
- * //_RB_ Don't think this comment is correct.  If this is for internal use only it should appear after all the public API functions and not start with FreeRTOS_.
- * Socket has had activity, reset the timer so it will not be closed
+ * //_RB_ Don't think this comment is correct.  If this is for internal use only
+ * it should appear after all the public API functions and not start with
+ * FreeRTOS_. Socket has had activity, reset the timer so it will not be closed
  * because of inactivity
  */
-#if ( ( ipconfigHAS_DEBUG_PRINTF != 0 ) || ( ipconfigHAS_PRINTF != 0 ) )
-    const char * FreeRTOS_GetTCPStateName( UBaseType_t ulState );
+#if( ( ipconfigHAS_DEBUG_PRINTF != 0 ) || ( ipconfigHAS_PRINTF != 0 ) )
+const char * FreeRTOS_GetTCPStateName( UBaseType_t ulState );
 #endif
 
-#if ( ipconfigDHCP_REGISTER_HOSTNAME == 1 )
+#if( ipconfigDHCP_REGISTER_HOSTNAME == 1 )
 
 /* DHCP has an option for clients to register their hostname.  It doesn't
  * have much use, except that a device can be found in a router along with its
  * name. If this option is used the callback below must be provided by the
  * application writer to return a const string, denoting the device's name. */
 /* Typically this function is defined in a user module. */
-    const char * pcApplicationHostnameHook( void );
+const char * pcApplicationHostnameHook( void );
 
 #endif /* ipconfigDHCP_REGISTER_HOSTNAME */
 
-
 /* This xApplicationGetRandomNumber() will set *pulNumber to a random number,
- * and return pdTRUE. When the random number generator is broken, it shall return
- * pdFALSE.
- * The function is defined in 'iot_secure_sockets.c'.
- * If that module is not included in the project, the application must provide an
+ * and return pdTRUE. When the random number generator is broken, it shall
+ * return pdFALSE. The function is defined in 'iot_secure_sockets.c'. If that
+ * module is not included in the project, the application must provide an
  * implementation of it.
  * The macro's ipconfigRAND32() and configRAND32() are not in use anymore. */
 
@@ -464,44 +463,46 @@ BaseType_t xIsNetworkDownEventPending( void );
  * be defined in a user module. */
 BaseType_t xApplicationGetRandomNumber( uint32_t * pulNumber );
 
-/** @brief The pointer to buffer with packet waiting for ARP resolution. This variable
- *  is defined in FreeRTOS_IP.c.
- *  This pointer is for internal use only. */
+/** @brief The pointer to buffer with packet waiting for ARP resolution. This
+ * variable is defined in FreeRTOS_IP.c. This pointer is for internal use only.
+ */
 extern NetworkBufferDescriptor_t * pxARPWaitingNetworkBuffer;
 
 /* For backward compatibility define old structure names to the newer equivalent
  * structure name. */
 #ifndef ipconfigENABLE_BACKWARD_COMPATIBILITY
-    #define ipconfigENABLE_BACKWARD_COMPATIBILITY    1
+    #define ipconfigENABLE_BACKWARD_COMPATIBILITY 1
 #endif
 
-#if ( ipconfigENABLE_BACKWARD_COMPATIBILITY == 1 )
-    #define xIPStackEvent_t               IPStackEvent_t
-    #define xNetworkBufferDescriptor_t    NetworkBufferDescriptor_t
-    #define xMACAddress_t                 MACAddress_t
-    #define xWinProperties_t              WinProperties_t
-    #define xSocket_t                     Socket_t
-    #define xSocketSet_t                  SocketSet_t
-    #define ipSIZE_OF_IP_HEADER           ipSIZE_OF_IPv4_HEADER
+#if( ipconfigENABLE_BACKWARD_COMPATIBILITY == 1 )
+    #define xIPStackEvent_t            IPStackEvent_t
+    #define xNetworkBufferDescriptor_t NetworkBufferDescriptor_t
+    #define xMACAddress_t              MACAddress_t
+    #define xWinProperties_t           WinProperties_t
+    #define xSocket_t                  Socket_t
+    #define xSocketSet_t               SocketSet_t
+    #define ipSIZE_OF_IP_HEADER        ipSIZE_OF_IPv4_HEADER
 
-/* Since August 2016, the public types and fields below have changed name:
- * abbreviations TCP/UDP are now written in capitals, and type names now end with "_t". */
-    #define FOnConnected                  FOnConnected_t
-    #define FOnTcpReceive                 FOnTCPReceive_t
-    #define FOnTcpSent                    FOnTCPSent_t
-    #define FOnUdpReceive                 FOnUDPReceive_t
-    #define FOnUdpSent                    FOnUDPSent_t
+    /* Since August 2016, the public types and fields below have changed name:
+     * abbreviations TCP/UDP are now written in capitals, and type names now end
+     * with "_t". */
+    #define FOnConnected               FOnConnected_t
+    #define FOnTcpReceive              FOnTCPReceive_t
+    #define FOnTcpSent                 FOnTCPSent_t
+    #define FOnUdpReceive              FOnUDPReceive_t
+    #define FOnUdpSent                 FOnUDPSent_t
 
-    #define pOnTcpConnected               pxOnTCPConnected
-    #define pOnTcpReceive                 pxOnTCPReceive
-    #define pOnTcpSent                    pxOnTCPSent
-    #define pOnUdpReceive                 pxOnUDPReceive
-    #define pOnUdpSent                    pxOnUDPSent
+    #define pOnTcpConnected            pxOnTCPConnected
+    #define pOnTcpReceive              pxOnTCPReceive
+    #define pOnTcpSent                 pxOnTCPSent
+    #define pOnUdpReceive              pxOnUDPReceive
+    #define pOnUdpSent                 pxOnUDPSent
 
-    #define FOnUdpSent                    FOnUDPSent_t
-    #define FOnTcpSent                    FOnTCPSent_t
+    #define FOnUdpSent                 FOnUDPSent_t
+    #define FOnTcpSent                 FOnTCPSent_t
 #endif /* ipconfigENABLE_BACKWARD_COMPATIBILITY */
 
+/* clang-format off */
 #if ( ipconfigHAS_PRINTF != 0 )
     extern void vPrintResourceStats( void );
 #else
